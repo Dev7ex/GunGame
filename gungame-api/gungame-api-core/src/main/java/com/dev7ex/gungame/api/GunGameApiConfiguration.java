aa4fa9c69c479f09eecfe53aa2d3ba9576886904
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2023-02-16 21:42:00 +0100

    Update GunGameApiConfiguration.java

--- gungame-api/gungame-api-core/src/main/java/com/dev7ex/gungame/api/GunGameApiConfiguration.java
+++ gungame-api/gungame-api-core/src/main/java/com/dev7ex/gungame/api/GunGameApiConfiguration.java
@@ -1,5 +1,7 @@
 package com.dev7ex.gungame.api;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * @author Dev7ex
  * @since 16.02.2023
@@ -7,5 +9,9 @@ package com.dev7ex.gungame.api;
 public interface GunGameApiConfiguration {
 
     String getPrefix();
+
+    String getString(@NotNull final String path);
+
+    boolean getBoolean(@NotNull final String path);
 
 }
