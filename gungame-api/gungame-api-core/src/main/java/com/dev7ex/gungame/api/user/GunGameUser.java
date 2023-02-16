c8a5ecaaea86dc772ee65ab6b6a835f2fc954094
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2023-02-16 20:19:34 +0100

    Add New Values

--- gungame-api/gungame-api-core/src/main/java/com/dev7ex/gungame/api/user/GunGameUser.java
+++ gungame-api/gungame-api-core/src/main/java/com/dev7ex/gungame/api/user/GunGameUser.java
@@ -10,10 +10,6 @@ public interface GunGameUser {
 
     UUID getUniqueId();
 
-    int getKills();
-
-    int getDeaths();
-
     String getName();
 
     GunGameUserConfiguration getConfiguration();
@@ -28,7 +24,28 @@ public interface GunGameUser {
 
     void setBuildMode(final boolean buildMode);
 
+    int getLevel();
+
+    void setLevel(final int level);
+
+    void decreaseLevel();
+
+    void increaseLevel();
+
+    int getKills();
+
     void setKills(final int kills);
 
+    void increaseKills();
+
+    int getDeaths();
+
     void setDeaths(final int deaths);
+
+    void increaseDeaths();
+
+    int getKillStreak();
+
+    void setKillStreak(final int killStreak);
+
 }
--- gungame-bukkit/src/main/java/com/dev7ex/gungame/user/User.java
+++ gungame-bukkit/src/main/java/com/dev7ex/gungame/user/User.java
@@ -1,11 +1,13 @@
 package com.dev7ex.gungame.user;
 
+import com.dev7ex.gungame.GunGamePlugin;
 import com.dev7ex.gungame.api.user.GunGameUser;
 import com.dev7ex.gungame.api.user.GunGameUserConfiguration;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 
 import java.util.UUID;
@@ -21,15 +23,75 @@ public class User implements GunGameUser {
 
     private final UUID uniqueId;
     private final String name;
-    private int level;
-    private int kills;
+    private GunGameUserConfiguration configuration;
     private boolean buildMode;
     private boolean spectator;
-    private GunGameUserConfiguration configuration;
+    private int level;
+    private int kills;
+    private int deaths;
+    private int killStreak;
 
     public User(final UUID uniqueId, final String name) {
         this.uniqueId = uniqueId;
         this.name = name;
+    }
+
+    @Override
+    public void setBuildMode(final boolean buildMode) {
+        if (buildMode) {
+            this.getEntity().setGameMode(GameMode.CREATIVE);
+            this.getEntity().getInventory().clear();
+            this.buildMode = true;
+            return;
+        }
+        this.getEntity().setGameMode(GameMode.ADVENTURE);
+        //TODO Set Equipment
+        this.buildMode = false;
+    }
+
+    @Override
+    public void setSpectator(final boolean spectator) {
+        if (spectator) {
+            this.getEntity().setGameMode(GameMode.CREATIVE);
+            this.getEntity().getInventory().clear();
+            Bukkit.getOnlinePlayers().forEach(player -> player.hidePlayer(GunGamePlugin.getInstance(), this.getEntity()));
+            this.spectator = true;
+            return;
+        }
+        this.getEntity().setGameMode(GameMode.ADVENTURE);
+        //TODO Set Equipment
+        Bukkit.getOnlinePlayers().forEach(player -> player.showPlayer(GunGamePlugin.getInstance(), this.getEntity()));
+        this.spectator = false;
+    }
+
+    @Override
+    public void decreaseLevel() {
+        if (this.level == 0) {
+            return;
+        }
+        if (this.level != 1) {
+            this.level = (int) Math.ceil((double) this.level / 2);
+
+        } else {
+            this.level--;
+        }
+        this.getEntity().setLevel(this.level);
+    }
+
+    @Override
+    public void increaseLevel() {
+        this.level++;
+        this.getEntity().setLevel(this.level);
+    }
+
+    @Override
+    public void increaseKills() {
+        this.kills++;
+    }
+
+    @Override
+    public void increaseDeaths() {
+        this.deaths++;
     }
 
     public Player getEntity() {
